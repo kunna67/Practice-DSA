3ca29f412a922808a324f80f13155ee7d9ba5ca7
Rabins Chaudhary <80695872+Rabins123@users.noreply.github.com>
2022-10-13 18:47:31 +0530

    Update 1-two-sum.cpp

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,19 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int a= 0;
+//         int b= 0;
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     a=i;
+//                     b=j;
+//                 }    
+//             }
+//         }  
+//         return {a,b};
+//     }
+// };
+
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int a= 0;
-        int b= 0;
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]==target)
+    
+      vector<int> twoSum(vector<int>& nums, int target) {
+                map<int,int> m;
+                vector<int> v;
+                int n= nums.size();
+                for(int i=0;i<n;i++)
                 {
-                    a=i;
-                    b=j;
-                }    
-            }
-        }  
-        return {a,b};
-    }
+                    
+                        int diff = target - nums[i];
+                        if(m.find(diff) != m.end())
+                        {
+                        auto p = m.find(diff);        
+                        v.push_back(p->second);
+                        v.push_back(i);
+                        }
+                        m.insert(make_pair(nums[i],i));
+                }
+          
+                return v;
+}
 };
