7bd57278a48dce698bd87c53222e15dd949ef5b8
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-06-15 10:58:25 +0530

    Time: 25 ms (29.33%), Space: 25.3 MB (56.54%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    TreeNode* prev=NULL;
+    void solve(TreeNode* root, int &mn){
+        if(root->left!=NULL){
+            solve(root->left,mn);
+        }
+        if(prev!=NULL){
+            mn=min(mn,abs(root->val - prev->val));
+        }
+        prev=root;
+        if(root->right!=NULL){
+            solve(root->right,mn);
+        }
+    }
+    int getMinimumDifference(TreeNode* root) {
+        int mn=INT_MAX;
+        solve(root,mn);
+        return mn;
+        // int mn=INT_MAX;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int sz=q.size();
+        //     while(sz--){
+        //         auto p=q.front();
+        //         q.pop();
+        //     if(p->left!=NULL){
+        //         int value=abs(p->val - p->left->val);
+        //         mn=min(mn,value);
+        //         q.push(p->left);
+        //     }
+        //     if(p->right!=NULL){
+        //         int value=abs(p->val - p->right->val);
+        //         mn=min(mn,value);
+        //         q.push(p->right);
+        //     }
+        //     }
+        // }
+        // return mn;
+        
+    }
+};
