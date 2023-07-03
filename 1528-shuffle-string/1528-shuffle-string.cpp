2d75923bde700762bf83ad2285d0f86f46cc33ab
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-07-03 23:24:38 +0530

    Time: 0 ms (100.00%), Space: 15 MB (89.39%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,17 +1,28 @@
+// class Solution {
+// public:
+//     string restoreString(string s, vector<int>& indices) { 
+//         vector<pair<int,char>>num;
+//         string str;
+//         str.resize(s.size());
+//         for(int i=0;i<indices.size();i++){
+//             num.push_back(make_pair(indices[i],s[i]));
+//         }
+//         sort(num.begin(),num.end());
+//         for(int i=0;i<indices.size();i++){
+//             str[i]=num[i].second;
+//         }
+//         return str;
+        
+//     }
+// };
 class Solution {
 public:
-    string restoreString(string s, vector<int>& indices) { 
-        vector<pair<int,char>>num;
-        string str;
-        str.resize(s.size());
-        for(int i=0;i<indices.size();i++){
-            num.push_back(make_pair(indices[i],s[i]));
+    string restoreString(string s, vector<int>& I) {
+         string sol=s;
+        for(int i = 0 ; i< I.size();i++){
+            sol[I[i]] = s[i];
         }
-        sort(num.begin(),num.end());
-        for(int i=0;i<indices.size();i++){
-            str[i]=num[i].second;
-        }
-        return str;
-        
+
+        return sol;
     }
 };
