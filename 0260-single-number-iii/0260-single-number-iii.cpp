1b21c93305125d866888637da0034ff3dd422361
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-08-26 12:28:20 +0530

    Time: 16 ms (14.22%), Space: 11.1 MB (9.62%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,15 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> singleNumber(vector<int>& nums) {
-//         map<int, int>m;
-//         for(int i=0; i<nums.size(); i++){m[nums[i]]++;}
-//         vector<int>ans;
-//         for(auto it: m){
-//             if(it.second!=2){ans.push_back(it.first);}
-//         }
-//         return ans;
-//     }
-// };
 
 class Solution {
 public:
