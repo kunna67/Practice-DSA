3b53dd17143b43b4d7bbeac204597a30f55fca77
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-08-29 15:50:44 +0530

    Time: 2 ms (88.60%), Space: 21 MB (13.03%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,3 +1,14 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream str(s);
+//         string word, ans = "";
+//         while(str >> word)
+//             ans = word + " " + ans;
+//         ans.pop_back();
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     string reverseWords(string s) {
