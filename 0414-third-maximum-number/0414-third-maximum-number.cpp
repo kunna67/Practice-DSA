5a1367f72be2b8c127bd67d1334f251079c59a86
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-10-22 10:09:15 +0530

    Time: 13 ms (8.40%), Space: 11.3 MB (7.59%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -14,15 +14,4 @@ public:
 
 
 
-           
-//            int thirdMax(vector<int>& nums) {
-//         set<int> set;
-//         for(int i=0;i<nums.size();i++)
-//             set.insert(nums[i]);     //Remove all duplicates
-//         vector<int> v(set.begin(),set.end());  //Convert to a vector
-//         sort(v.begin(),v.end(),greater<int>());  //Sort in descending order
-//         if(v.size()>=3) return v[2];  //If there are at least 3 elements, then return the third element, else return the first element
-//         else return v[0];
-//     }
-
-
+       
