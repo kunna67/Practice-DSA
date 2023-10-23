10c5775e859bf86e3bd5b1f2e9634db7f51225ee
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-10-23 23:22:00 +0530

    Time: 3 ms (44.29%), Space: 6.7 MB (13.81%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        return s;
+        
+    }
+};
