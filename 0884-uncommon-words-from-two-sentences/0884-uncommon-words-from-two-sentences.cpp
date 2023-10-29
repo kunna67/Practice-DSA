25620fcaf593714ad328d0e21dedbdd7c4709dca
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-10-29 22:25:13 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (42.72%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string, int > count;
+        istringstream iss(s1 + " " + s2);
+        while(iss>>s1) count[s1]++;
+        vector<string>res;
+        for(auto w: count)
+            if(w.second == 1)
+                res.push_back(w.first);
+        return res;
+        
+    }
+};
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> uncommonFromSentences(string A, string B) {
+//         unordered_map<string, int> count;
+//         istringstream iss(A + " " + B);
+//         while (iss >> A) count[A]++;
+//         vector<string>res;
+//         for (auto w: count)
+//             if (w.second == 1)
+//                 res.push_back(w.first);
+//         return res;
+//     }
+// };
