d7f6aa97db83999c2b16f2f7d9f5b08900c854e9
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-11-03 23:55:25 +0530

    Added solution - LeetHub

---
+++ C++ Hello World - GFG/c-hello-world.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello 
+        cout<<"Hello World";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout<<endl;
+    return 0;
+}
+// } Driver Code Ends
