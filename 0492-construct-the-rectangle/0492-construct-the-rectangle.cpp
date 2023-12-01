6336ef75d71ceddd4b988235faf7b8bbadf8c896
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-12-01 12:06:34 +0530

    Time: 3 ms (33.89%), Space: 6.5 MB (57.67%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        vector<int> v;
+        int root;
+        root=sqrt(area);
+
+        for(root; root>0;root--)
+        {
+            if(area%root==0)
+            {
+                v.push_back(area/root);
+                v.push_back(root);
+                break;
+            }
+        }
+        return v;
+    }
+
+};
