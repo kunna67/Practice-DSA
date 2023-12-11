76f2bdb836e1f96bc10fe2ce0a30b2d937bd064f
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-12-11 10:38:43 +0530

    Time: 12 ms (46.74%), Space: 14.7 MB (6.53%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+
+      
+        int n=arr.size();
+       int  y=(n*25)/100;
+
+       // MAP KO FILL KAR RAHE HN
+        map<int,int>mp;
+        for(auto it : arr)
+        {
+            mp[it]++;
+        }
+
+        // array KE ANDAR TRAVERSE KAR RAHE  AUR DEKH RAHE KI ARRAY KA ith ELEMENT KI FREQUENCY MAP KE ANDAR 25% (Y) SE ZADA HAI YA NAHI.
+       for(int i=0;i<n;i++)
+       {
+          if(mp[arr[i]]>y)
+          {
+              return arr[i];
+          }
+       }
+       return +1;
+    }
+};
