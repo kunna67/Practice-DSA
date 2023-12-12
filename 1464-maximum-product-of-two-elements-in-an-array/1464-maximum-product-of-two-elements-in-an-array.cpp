982d1ae2108ed7e199ceea25c0c39180b91dca2e
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-12-12 11:15:43 +0530

    Time: 2 ms (86.12%), Space: 10.5 MB (25.23%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,32 +1,51 @@
+// class Solution {
+// public:
+// int maxnum1(vector<int>& nums){
+//     int n=nums.size()-1;
+//     sort(nums.begin(),nums.end());
+//     return nums[n];
+
+// }
+// int maxnum2(vector<int>& nums){
+//     int n=nums.size()-1;
+//     sort(nums.begin(),nums.end());
+//     return nums[n-1];
+
+// }
+//     int maxProduct(vector<int>& nums) {
+//         int n1=maxnum1(nums);
+//         int n2=maxnum2(nums);
+//         int prod=(n1-1)*(n2-1);
+//           int maxi;
+//         for(int i=0;i<nums.size();i++){
+//           maxi=0;
+//             for(int j=0;j<nums.size();j++){
+//                 maxi=max(prod,maxi);
+                
+                
+
+//             }
+//         }
+//         return maxi;
+        
+//     }
+// };
+
+// Using no sorting method
 class Solution {
 public:
-int maxnum1(vector<int>& nums){
-    int n=nums.size()-1;
-    sort(nums.begin(),nums.end());
-    return nums[n];
-
-}
-int maxnum2(vector<int>& nums){
-    int n=nums.size()-1;
-    sort(nums.begin(),nums.end());
-    return nums[n-1];
-
-}
     int maxProduct(vector<int>& nums) {
-        int n1=maxnum1(nums);
-        int n2=maxnum2(nums);
-        int prod=(n1-1)*(n2-1);
-          int maxi;
-        for(int i=0;i<nums.size();i++){
-          maxi=0;
-            for(int j=0;j<nums.size();j++){
-                maxi=max(prod,maxi);
-                
-                
-
+        // Intuition
+        // The idea in here is pretty simple. We will find the maximum two elemens of the nums vector and then return the required answer.
+        int firstMax  = -1;
+        int secondMax = -1;
+        for(auto &ele : nums){
+            if(ele > firstMax) {
+                secondMax = firstMax;
+                firstMax  = ele;
             }
+            else if(ele > secondMax) secondMax = ele;
         }
-        return maxi;
-        
+        return (firstMax - 1) * (secondMax - 1);
     }
 };
