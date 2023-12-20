27ade070b9b806dd4d81bea0326e69997b282669
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-12-20 08:54:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modified Game of Nim/modified-game-of-nim.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int res = A[0];
+        for(int i=1;i<n;i++){
+            res^=A[i];
+        }
+        if(res==0)return 1;
+        if(n%2==0)return 1;
+        else return 2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
