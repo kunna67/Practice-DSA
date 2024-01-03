c799f9c5957264489972eb2cc1e4573e9e2705c3
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2024-01-03 23:40:07 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (23.26%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {  //Time : O(N!*N)
+public:
+    
+     void solve(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal, int n){
+        
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row = 0; row<n; row++){
+            
+            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0){
+                
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1 + col - row] = 1;
+                solve(col+1,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
+                
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] = 0;
+                
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+         vector<vector<string>> res;
+        vector<string> board(n);
+        
+        string s(n,'.');
+        
+        for(int i=0;i<n;i++){
+            board[i] = s;
+        }
+        
+        vector<int> leftRow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
+        solve(0,board,res,leftRow,upperDiagonal,lowerDiagonal,n);
+        
+        int ans = res.size();
+        return ans;
+        
+    }
+};
