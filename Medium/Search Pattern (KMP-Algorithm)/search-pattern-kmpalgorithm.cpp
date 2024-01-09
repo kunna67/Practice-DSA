2c860bb55d728139709f35abc7e078cb59e35bec
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2024-01-09 23:56:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code here
+            vector<int>v;
+            int n=pat.size();
+            for(int i=0;i<txt.size();i++){
+                if(txt[i]==pat[0]){
+                    if(txt.substr(i,n)==pat){
+                        v.push_back(i+1);
+                    }
+                }
+            }
+            return v;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
