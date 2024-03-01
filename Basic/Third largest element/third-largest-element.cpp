f1a0805d4a7aa34ef398913ecaf7f02ea4cbc659
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2024-03-01 11:09:18 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Third largest element/third-largest-element.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         //Your code here
+         sort(a,a+n);
+         int ans=a[n-3];
+         return ans;
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}   
+// } Driver Code Ends
