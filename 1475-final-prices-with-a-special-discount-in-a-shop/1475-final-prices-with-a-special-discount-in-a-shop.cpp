3caea4044c38b19fee656897de1c08fd624ff0ed
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2024-12-18 18:59:32 +0530

    Time: 3 ms (16.00%), Space: 14.2 MB (9.86%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> ans(prices.size());
+        stack<int> st;
+        for(int i =prices.size()-1;i>=0;i--){
+            while(!st.empty()&&st.top()>prices[i])
+                st.pop();
+            if(st.empty()){
+                ans[i]=prices[i];
+                st.push(prices[i]);
+            }
+            else{
+                ans[i]=prices[i]-st.top();
+                st.push(prices[i]);
+            }
+        }
+        return ans;
+    }
+};
